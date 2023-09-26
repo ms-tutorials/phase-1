db06f492f810b79e977cda1e61f80e50fe48df39
devsamuelmwangi <hirewebappdeveloper@gmail.com>
2023-09-26 19:32:23 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#Design Pattern/Clean code
+#DRY - DO NOT REPEAT YOURSELF
+
+j = 0
+for i in range(1,21):
+    if i % 2 != 0:
+        continue
+    j += i
+print(j)
+
+#git add, git commit - stage//snapshot
+
